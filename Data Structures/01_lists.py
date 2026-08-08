dbef3ede5982024ce97bb31c3a07c46b7e7fa37d
Om Toke <omtoke21@gmail.com>
2026-08-08 21:04:20 +0530

    Add list operations for student management

---
+++ Data Structures/01_lists.py
@@ -0,0 +1,13 @@
+# Lists
+
+students = ["Om", "Rahul", "Amit", "Sneha"]
+
+students.append("Priya")
+
+print("Students:", students)
+print("First student:", students[0])
+print("Number of students:", len(students))
+
+students.remove("Amit")
+
+print("Updated list:", students)
