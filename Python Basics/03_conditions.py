a1878d9f83373c80b511727db0367b32e0dfa702
Om Toke <omtoke21@gmail.com>
2026-08-08 20:59:34 +0530

    Add conditional statements for grading system

---
+++ Python Basics/03_conditions.py
@@ -0,0 +1,17 @@
+# Conditional Statements
+
+marks = 78
+
+if marks >= 90:
+    grade = "A+"
+elif marks >= 75:
+    grade = "A"
+elif marks >= 60:
+    grade = "B"
+elif marks >= 40:
+    grade = "C"
+else:
+    grade = "Fail"
+
+print("Marks:", marks)
+print("Grade:", grade)
