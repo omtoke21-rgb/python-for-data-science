4c3af608bb004a9ca210889db637ea3bab731846
Om Toke <omtoke21@gmail.com>
2026-08-08 20:49:03 +0530

    Add function to calculate average of a list

---
+++ 05_functions.py
@@ -0,0 +1,12 @@
+# Functions
+
+def calculate_average(numbers):
+    return sum(numbers) / len(numbers)
+
+
+marks = [78, 85, 91, 67, 73]
+
+average = calculate_average(marks)
+
+print("Marks:", marks)
+print("Average:", average)
